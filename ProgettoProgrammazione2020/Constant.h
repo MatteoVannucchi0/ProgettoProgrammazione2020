30e02c12ba297b6b2e992307b41efc8ec59ab317
matteo00111 <matteo.vannucchi11@gmail.com>
2020-02-10 02:39:09 +0100

    Commit test

--- ProgettoProgrammazione2020/Constant.h
+++ ProgettoProgrammazione2020/Constant.h
@@ -1,14 +1,25 @@
 #pragma once
+#include <string>
+using namespace std;
+
 const int MAP_HEIGHT = 25;
-const int MAP_WIDTH = 50;
+const int MAP_WIDTH = 51;
 const int MAP_OFFSET_Y = 0;
 const int MAP_OFFSET_X = 5;
+
 const int MAIN_MENU_OFFSET_X = 25;
 const int MAIN_MENU_OFFSET_Y = 0;
 const int MAIN_MENU_DISTANCE_BETWEEN_OPTION = 4;
 const string MAIN_MENU_OPTION[] = { "PLAY", "INSTRUCTION", "BEST SCORE", "EXIT" };
+const int MAIN_MENU_NUMBER_OF_OPTION = 4;
+const char MAIN_MENU_KEY_ONE_OPTION_DOWN = 's';
+const char MAIN_MENU_KEY_ONE_OPTION_UP = 'w';
+const char MAIN_MENU_KEY_SELECTOR = '\n';
+const int  MAIN_MENU_KEY_EXIT = 27; // il 27 è per il tasto Esc
+
 #define WallPair 1
 #define ObstaclePair 2
 #define AirPair 3
+#define PitPair 4
 #define SelectedMenuOptionPair 99
 #define StandardWriteColorPair 100
--- ProgettoProgrammazione2020/MainMenu.h
+++ ProgettoProgrammazione2020/MainMenu.h
@@ -1,16 +1,57 @@
 #pragma once
+#include <string>
 #include "Screen.h"
 #include "Constant.h"
 using namespace std;
 
+enum class MenuOption { 
+	Play,
+	Instruction,
+	Bestscore,
+	Exit
+};
+/*	 Perchè aggiungere un Enum quando semplicemente una variabile interna alla classe (in questo caso selectedOption) tiene già traccia della opzione su cui siamo?
+	 Lo faccio per fare in modo che il codice sia più leggibile, cosi invece che avere tipo selectedOption == 1 ? abbiamo GetMenuOptionState() == MenuOption::Instruction, 
+	 poi cosi che se si volesse cambiare in futuro le opzioni, e quindi le relative azioni da fare in caso vengano cliccati
+	 andrebbe cambiato quattro cose: 
+		 1 - la variabile (array di stringhe) (costante o no) che tiene traccia delle varie opzioni da stampare
+		 2 - la dichiarazione dell' Enum (aggiungendo o togliendo delle opzioni)
+		 3 - la funzione GetMenuOptionSelected che andrebbe cambiata aggiungendo o togliendo opzioni disponibili
+		 4 - aggiungere le funzioni relative a quando si seleziona l'opzione (il DrawMenu e OnMenuSelected).
+	 In caso utilizzazi un semplice numero (selectedOption) per tutto se volessi aggiungere qualcosa dovrei cambiare si meno cose, ma in per solo la 2 è (in caso di codice lungo) molto più lunga da fare: 
+		 1 - la variabile che tiene traccia delle varie opzioni da stampare
+		 2 - ogni singolo controllo per vedere in quale "stato" siamo, andando ad aggiungere quindi in mezzo al codice numeri apparentemente random 
+					(Es: Poniamo che abbiamo tre opzioni iniziali Play, Bestscore e exit e vogliamo aggiungere Instruction in mezzo fra play e Bestscore. Quello che dobbiamo fare è andare a cambiare ogni singolo numero apparentemente random che andava a controllare in quale "stato" eravamo)
+		 3 - aggiungere le funzioni relative a quando si seleziona l'opzione. (il DrawMenu e OnMenuSelected).
+
+*/
+
 class MainMenu {
 protected:
-	Screen MenuScreen;
-	int SelectedOption;
-public:
-	MainMenu();
+	
 
-	void DrawMenu();
+	Screen MenuScreen;
+	int selectedOption; // 0 == PLAY, 1 == Instruction, 2 == Best score, 3 == Exit
+
+	bool isOnInternalMenu;
+	bool hasClickedOnPlay;
+
+	void Draw();
 
 	void ManageInput();
+
+	void OnSelection();
+
+	void OnExitMenuSelected();
+	void OnPlayMenuSelected();
+	void OnInstructionMenuSelected();
+	void OnBestScoreMenuSelected();
+
+	void DrawInstructionMenu();
+	void DrawBestScoreMenu();
+
+	MenuOption GetMenuOptionSelected();
+public:
+	MainMenu();
+	bool MenuUpdateCall();
 };
